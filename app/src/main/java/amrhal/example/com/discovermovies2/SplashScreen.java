9710e2c7ecd227b75f7d18809764ba01d2088563
Amr Halawani <amrhalawani19@gmail.com>
2018-05-04 13:56:24 +0200

    finish the Project r2

---
+++ app/src/main/java/amrhal/example/com/discovermovies2/SplashScreen.java
@@ -0,0 +1,34 @@
+package amrhal.example.com.discovermovies2;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        finishAfter(3300);
+    }
+
+
+    private void finishAfter(int SPLASH_TIME_OUT) {
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent in = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(in);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
